af7b617c8019c75db77c7f6cc18cb83cbc5580bd
Eduard Schwarzkopf <eschwarzkopf@evoila.de>
2023-03-08 10:01:16 +0100

    adjusted tsconfig

--- .projenrc.js
+++ .projenrc.js
@@ -1,29 +1,40 @@
-const { web } = require('projen');
-const { GithubCredentials } = require('projen/lib/github');
-const { NpmAccess, NodePackageManager } = require('projen/lib/javascript');
+const { web } = require("projen");
+const { GithubCredentials } = require("projen/lib/github");
+const { NpmAccess, NodePackageManager } = require("projen/lib/javascript");
 
 const project = new web.NextJsTypeScriptProject({
-  defaultReleaseBranch: 'main',
-  name: 'event-system-frontend-web',
-  srcdir: 'src',
-  packageManager: NodePackageManager.NPM,
-  githubOptions: {
-    mergify: false,
-    // projenCredentials: GithubCredentials.fromApp(),
-  },
-  deps: [
-    'swr',
-    '@next/font',
-  ],
+    defaultReleaseBranch: "main",
+    name: "event-system-frontend-web",
+    srcdir: ".",
+    packageManager: NodePackageManager.NPM,
+    githubOptions: {
+        mergify: false,
+        // projenCredentials: GithubCredentials.fromApp(),
+    },
+    deps: ["swr", "@next/font", "next-auth"],
+    tsconfig: {
+        compilerOptions: {
+            rootDir: ".",
+            outDir: ".",
+            baseUrl: ".",
+            paths: {
+                "@/*": ["./*"],
+            },
+            plugins: [
+                {
+                    name: "next",
+                },
+            ],
+        },
+        include: ["**/*.tsx", ".next/types/**/*.ts"],
+    },
 });
-project.tryFindFile('tailwind.config.json').obj = {
-  content: [
-    './src/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+project.tryFindFile("tailwind.config.json").obj = {
+    content: ["./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 };
 
 project.synth();
