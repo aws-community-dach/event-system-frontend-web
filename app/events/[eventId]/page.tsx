3011795d3909e6825c6cb2beaf66412c22d1fded
Eduard Schwarzkopf <eschwarzkopf@evoila.de>
2023-03-09 09:33:49 +0100

    page for single event added

---
+++ app/events/[eventId]/page.tsx
@@ -0,0 +1,21 @@
+import { EventType } from "@/types/EventType";
+
+async function getData(eventId: string) {
+    const res = await fetch(
+        `http://localhost:3000/api/samples/events/${eventId}`
+    );
+    if (!res.ok) {
+        throw new Error("Failed to fetch data");
+    }
+
+    return res.json();
+}
+
+export default async function Page({
+    params,
+}: {
+    params: { eventId: string };
+}) {
+    const event: EventType = await getData(params.eventId);
+    return <div>{JSON.stringify(event)}</div>;
+}
