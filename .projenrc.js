0a97a2f8b64976f84b46f35e7ebd86c8ae85706f
Eduard Schwarzkopf <eschwarzkopf@evoila.de>
2023-03-09 15:47:37 +0100

    lint

--- .projenrc.js
+++ .projenrc.js
@@ -1,41 +1,41 @@
-const { web } = require("projen");
-const { GithubCredentials } = require("projen/lib/github");
-const { NpmAccess, NodePackageManager } = require("projen/lib/javascript");
+const { web } = require('projen');
+const { GithubCredentials } = require('projen/lib/github');
+const { NpmAccess, NodePackageManager } = require('projen/lib/javascript');
 
 const project = new web.NextJsTypeScriptProject({
-    defaultReleaseBranch: "main",
-    name: "event-system-frontend-web",
-    srcdir: ".",
-    packageManager: NodePackageManager.NPM,
-    githubOptions: {
-        mergify: false,
-        // projenCredentials: GithubCredentials.fromApp(),
-    },
-    gitignore: [".env.local", ".vscode"],
-    deps: ["swr", "@next/font", "next-auth"],
-    tsconfig: {
-        compilerOptions: {
-            rootDir: ".",
-            outDir: ".",
-            baseUrl: ".",
-            paths: {
-                "@/*": ["./*"],
-            },
-            plugins: [
-                {
-                    name: "next",
-                },
-            ],
+  defaultReleaseBranch: 'main',
+  name: 'event-system-frontend-web',
+  srcdir: '.',
+  packageManager: NodePackageManager.NPM,
+  githubOptions: {
+    mergify: false,
+    // projenCredentials: GithubCredentials.fromApp(),
+  },
+  gitignore: ['.env.local', '.vscode'],
+  deps: ['swr', '@next/font', 'next-auth'],
+  tsconfig: {
+    compilerOptions: {
+      rootDir: '.',
+      outDir: '.',
+      baseUrl: '.',
+      paths: {
+        '@/*': ['./*'],
+      },
+      plugins: [
+        {
+          name: 'next',
         },
-        include: ["**/*.tsx", ".next/types/**/*.ts"],
+      ],
     },
+    include: ['**/*.tsx', '.next/types/**/*.ts'],
+  },
 });
-project.tryFindFile("tailwind.config.json").obj = {
-    content: ["./src/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
+project.tryFindFile('tailwind.config.json').obj = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 };
 
 project.synth();
