133c6c5c5306d5068e10265a27df95893e2d8d63
Eduard Schwarzkopf <eschwarzkopf@evoila.de>
2023-03-20 10:04:37 +0100

    added participant data

---
+++ data/participants.ts
@@ -0,0 +1,26 @@
+export const participantList = [
+  {
+    id: 1,
+    name: 'John Doesnt',
+    displayName: 'John',
+    email: 'john@examples.com',
+  },
+  {
+    id: 2,
+    name: 'Karl Darl',
+    displayName: 'Karl',
+    email: 'karl@darl.de',
+  },
+  {
+    id: 3,
+    name: 'Peter Schmidt',
+    displayName: 'Peter',
+    email: 'karl@gustaf.de',
+  },
+  {
+    id: 4,
+    name: 'Felix Grauer',
+    displayName: 'Felix',
+    email: 'felix@Grauer.de',
+  },
+];
