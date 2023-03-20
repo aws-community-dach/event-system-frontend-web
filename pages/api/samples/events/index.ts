7de54b17660b61673823e5108057e64ef40a8ef0
Eduard Schwarzkopf <eschwarzkopf@evoila.de>
2023-03-20 10:04:54 +0100

    moved

--- pages/api/samples/events/index.ts
+++ pages/api/samples/events/index.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiResponse, NextApiRequest } from 'next';
-import { eventList } from '@/data/event';
+import { eventList } from '@/data/events';
 import type { EventType } from '@/types/EventType';
 
 type ResponseType = EventType[];
