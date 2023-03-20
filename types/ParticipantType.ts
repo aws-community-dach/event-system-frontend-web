96d2d8618c0cdd2a23c8037ef90258c9b67768df
Eduard Schwarzkopf <eschwarzkopf@evoila.de>
2023-03-20 10:33:43 +0100

    added form types

--- types/ParticipantType.ts
+++ types/ParticipantType.ts
@@ -5,8 +5,12 @@ export type ParticipantType = {
   email: string;
 };
 
-export type ParticipantFormElementsType = {
+type ParticipantInputElementsType = HTMLFormControlsCollection & {
   name: HTMLInputElement;
   displayName: HTMLInputElement;
   email: HTMLInputElement;
+};
+
+export type ParticipantFormType = HTMLFormElement & {
+  readonly elements: ParticipantInputElementsType;
 };
