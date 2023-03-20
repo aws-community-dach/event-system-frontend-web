3f815da30b5fa6cf276c4f790aedd481ad68626e
Eduard Schwarzkopf <eschwarzkopf@evoila.de>
2023-03-20 10:05:46 +0100

    added participant type

---
+++ types/ParticipantType.ts
@@ -0,0 +1,12 @@
+export type ParticipantType = {
+  id: number;
+  name: string;
+  displayName: string;
+  email: string;
+};
+
+export type ParticipantFormElementsType = {
+  name: HTMLInputElement;
+  displayName: HTMLInputElement;
+  email: HTMLInputElement;
+};
