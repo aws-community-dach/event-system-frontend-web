3f40394dc31770e33ac46af2798b1cb61a21245f
Eduard Schwarzkopf <eschwarzkopf@evoila.de>
2023-03-20 10:05:38 +0100

    added event type

--- types/EventType.ts
+++ types/EventType.ts
@@ -1,7 +1,7 @@
 export type EventType = {
-    id: number;
-    name: string;
-    date: string;
-    location: string;
-    organizerID: number;
+  id: number;
+  name: string;
+  date: string;
+  location: string;
+  organizerID: number;
 };
