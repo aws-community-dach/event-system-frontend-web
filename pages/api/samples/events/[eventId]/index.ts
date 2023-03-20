8c7411b5d6c3b701b224f402bc17c2f2df99c416
Eduard Schwarzkopf <eschwarzkopf@evoila.de>
2023-03-20 10:05:03 +0100

    moved

---
+++ pages/api/samples/events/[eventId]/index.ts
@@ -0,0 +1,19 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiResponse, NextApiRequest } from 'next';
+import { eventList } from '@/data/events';
+import type { EventType } from '@/types/EventType';
+import { NotFoundError } from '@/types/ResponseTypes';
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<EventType | NotFoundError>
+) {
+  const { eventId } = req.query;
+  const id = Number(eventId) - 1;
+  const event = eventList[id];
+
+  if (!event) {
+    return res.status(404).json({ message: 'Not Found' });
+  }
+  res.status(200).json(event);
+}
