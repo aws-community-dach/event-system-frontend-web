2d9ecb9708100a93b8530417285815eb15e1c42c
Eduard Schwarzkopf <eschwarzkopf@evoila.de>
2023-03-22 19:09:20 +0100

    removed sample data api call

--- app/events/[eventId]/page.tsx
+++ app/events/[eventId]/page.tsx
@@ -1,22 +1,13 @@
 import EventForm from '@/components/EventsForm';
-import { EventType } from '@/types/EventType';
-
-async function getData(eventId: string) {
-  const res = await fetch(
-    `http://localhost:3000/api/samples/events/${eventId}`
-  );
-  if (!res.ok) {
-    throw new Error('Failed to fetch data');
-  }
-
-  return res.json();
-}
 
 export default async function Page({
   params,
 }: {
   params: { eventId: string };
 }) {
-  const event: EventType = await getData(params.eventId);
-  return <EventForm eventId={event.id} />;
+  return (
+    <>
+      <EventForm eventId={params.eventId} />;
+    </>
+  );
 }
