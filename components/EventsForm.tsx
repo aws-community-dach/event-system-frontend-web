5538059cf1f5d1c611b4af100e9d59b3084657d1
Eduard Schwarzkopf <eschwarzkopf@evoila.de>
2023-03-22 19:09:30 +0100

    use API service

--- components/EventsForm.tsx
+++ components/EventsForm.tsx
@@ -1,8 +1,9 @@
 'use client';
 
+import { ParticipantService } from '@/service/events/ParticipantService';
 import { ParticipantFormType } from '@/types/ParticipantType';
 
-export default function EventForm({ eventId }: { eventId: number }) {
+export default function EventForm({ eventId }: { eventId: string }) {
   const handleSubmit = async (event: React.FormEvent<ParticipantFormType>) => {
     event.preventDefault();
 
@@ -14,30 +15,13 @@ export default function EventForm({ eventId }: { eventId: number }) {
       email: target.email.value,
     };
 
-    const JSONdata = JSON.stringify(data);
-
-    const endpoint = `/api/samples/events/${eventId}/participants`;
-
-    const options = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSONdata,
-    };
-
-    const response = await fetch(endpoint, options);
-
-    const result = await response.json();
-    alert(
-      `Is this your submitted data: \nName: ${result.name}\nDisplay Name: ${result.displayName}\nEmail: ${result.email}`
-    );
+    ParticipantService.add(eventId, data);
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor='participant-name'>Name:</label>
-      <input type='text' id='participant-name' required />
+      <label htmlFor='name'>Name:</label>
+      <input type='text' id='name' required />
 
       <label htmlFor='displayName'>Display Name:</label>
       <input type='text' id='displayName' required />
