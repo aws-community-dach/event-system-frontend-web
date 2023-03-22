0a54c93861ac5f0c234748ccb8106186839eda51
Eduard Schwarzkopf <eschwarzkopf@evoila.de>
2023-03-22 19:08:32 +0100

    make variable public for client

--- process.d.ts
+++ process.d.ts
@@ -1,8 +1,9 @@
 declare namespace NodeJS {
-    export interface ProcessEnv {
-        COGNITO_CLIENT_ID: string;
-        COGNITO_CLIENT_SECRET: string;
-        COGNITO_ISSUER: string;
-        AUTH_SECRET: string;
-    }
+  export interface ProcessEnv {
+    COGNITO_CLIENT_ID: string;
+    COGNITO_CLIENT_SECRET: string;
+    COGNITO_ISSUER: string;
+    AUTH_SECRET: string;
+    NEXT_PUBLIC_API_BASE_URL: string;
+  }
 }
