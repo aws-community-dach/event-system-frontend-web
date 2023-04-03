1b5c1ecdd79af5832b3712ec951ebceea6f9c847
Eduard Schwarzkopf <eschwarzkopf@evoila.de>
2023-04-03 10:34:49 +0200

    added navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css';
+import Nav from './Nav';
 
 export default function RootLayout({
   children,
@@ -12,7 +13,10 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body className={`mx-4 md:mx-48 xl:mx-96 font-sans bg-gray-200`}>
+        <Nav />
+        {children}
+      </body>
     </html>
   );
 }
