b3590af528197ea114aec4d465488c6a93985550
Eduard Schwarzkopf <eschwarzkopf@evoila.de>
2023-04-04 14:33:18 +0200

    added event card

--- app/page.tsx
+++ app/page.tsx
@@ -12,9 +12,13 @@ export default async function Home() {
   const events: EventType[] = await getData();
   return (
     <>
-      {events?.map((event) => (
-        <EventCard key={event.id} event={event} />
-      ))}
+      <div className='mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8'>
+        <div className='mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-1 lg:grid-cols-3 xl:gap-x-8'>
+          {events?.map((event) => (
+            <EventCard key={event.id} event={event} />
+          ))}
+        </div>
+      </div>
     </>
   );
 }
--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -1,19 +1,27 @@
 'use client';
 import { EventType } from '@/types/EventType';
 import Link from 'next/link';
-import { useState } from 'react';
+import Button from './Button';
 
 export default function EventCard({ event }: { event: EventType }) {
   return (
-    <div className='relative overflow-hidden mb-8'>
-      <div className='bg-white rounded'>
-        <div className='px-6 py-4'>
-          <div className='font-bold text-xl mb-2'>{event.name}</div>
-          <p className='text-gray-700 text-base'>{event.summary}</p>
+    <div className='min-h-80 aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75'>
+      <div className='bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4'>
+        <div className='sm:flex sm:items-start'>
+          <div className='mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left'>
+            {event.name}
+            <div className='mt-2'>
+              <p className='text-sm text-gray-500'>{event.summary}</p>
+            </div>
+            <div>
+              <Link href={`/events/${event.id}`}>Mehr erfahren</Link>
+            </div>
+            <Button>Registrieren</Button>
+          </div>
         </div>
-        <Link href={`/events/${event.id}`}>Mehr erfahren</Link>
+      </div>
 
-        <div>Registrieren</div>
+      <div className='bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6'>
         <div>{event.date}</div>
         <div>{event.location}</div>
       </div>
