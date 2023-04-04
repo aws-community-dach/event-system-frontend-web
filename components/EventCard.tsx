d84092bb1b65e24fffeb83ee8d85c56d4588fe0c
Eduard Schwarzkopf <eschwarzkopf@evoila.de>
2023-04-04 15:05:32 +0200

    more styling

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -4,26 +4,29 @@ import Link from 'next/link';
 import Button from './Button';
 
 export default function EventCard({ event }: { event: EventType }) {
+  const date = new Date(event.date).toLocaleDateString('de-DE');
+
   return (
-    <div className='min-h-80 aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75'>
-      <div className='bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4'>
+    <div className='bg-white min-h-80 w-full rounded-md'>
+      <div className='pb-4 pt-5 sm:p-6 sm:pb-4'>
         <div className='sm:flex sm:items-start'>
-          <div className='mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left'>
-            {event.name}
+          <div className='mt-3 sm:mt-0'>
+            <div className=' text-xl '>{event.name}</div>
             <div className='mt-2'>
               <p className='text-sm text-gray-500'>{event.summary}</p>
             </div>
-            <div>
+            <div className='my-6'>
               <Link href={`/events/${event.id}`}>Mehr erfahren</Link>
             </div>
-            <Button>Registrieren</Button>
+            <Button className='w-full'>Registrieren</Button>
           </div>
         </div>
-      </div>
-
-      <div className='bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6'>
-        <div>{event.date}</div>
-        <div>{event.location}</div>
+        <div className='py-3 sm:flex'>
+          <div className='flex flex-col'>
+            <div>{date}</div>
+            <div>{event.location}</div>
+          </div>
+        </div>
       </div>
     </div>
   );
