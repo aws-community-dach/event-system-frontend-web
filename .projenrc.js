1a946320217f18063d5f5f073ded6839a002e8dc
Eduard Schwarzkopf <eschwarzkopf@evoila.de>
2023-04-04 14:32:57 +0200

    updated tailwind colors

--- .projenrc.js
+++ .projenrc.js
@@ -50,7 +50,21 @@ project.tryFindFile('tailwind.config.json').obj = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: '#cffafe',
+          200: '#a5f3fc',
+          300: '#67e8f9',
+          400: '#22d3ee',
+          500: '#06b6d4',
+          600: '#0891b2',
+          700: '#0e7490',
+          800: '#155e75',
+          900: '#164e63',
+        },
+      },
+    },
   },
   plugins: [],
 };
