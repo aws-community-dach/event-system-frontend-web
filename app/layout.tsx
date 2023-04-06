fc6c6abe2cd6175ae17a710c75e2a2f27c353262
Eduard Schwarzkopf <eschwarzkopf@evoila.de>
2023-04-06 13:53:40 +0200

    removed headed

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,13 +19,6 @@ export default async function RootLayout({
       <body className='h-full'>
         <div className='min-h-full'>
           <Nav session={session} />
-          <header className='bg-white shadow'>
-            <div className='mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8'>
-              <h1 className='text-3xl font-bold tracking-tight text-gray-900'>
-                Dashboard
-              </h1>
-            </div>
-          </header>
           <main>
             <div className='mx-auto max-w-7xl py-6 sm:px-6 lg:px-8'>
               {children}
