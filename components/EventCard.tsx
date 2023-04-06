d5a04f33529cf3d6059de437b7be643410658004
Eduard Schwarzkopf <eschwarzkopf@evoila.de>
2023-04-06 13:08:30 +0200

    more styling

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -7,11 +7,11 @@ export default function EventCard({ event }: { event: EventType }) {
   const date = new Date(event.date).toLocaleDateString('de-DE');
 
   return (
-    <div className='bg-white min-h-80 w-full rounded-md'>
+    <div className='px-4 bg-white min-h-80 w-full rounded-lg'>
       <div className='pb-4 pt-5 sm:p-6 sm:pb-4'>
         <div className='sm:flex sm:items-start'>
           <div className='mt-3 sm:mt-0'>
-            <div className=' text-xl '>{event.name}</div>
+            <div className=' text-2xl '>{event.name}</div>
             <div className='mt-2'>
               <p className='text-sm text-gray-500'>{event.summary}</p>
             </div>
@@ -21,7 +21,7 @@ export default function EventCard({ event }: { event: EventType }) {
             <Button className='w-full'>Registrieren</Button>
           </div>
         </div>
-        <div className='py-3 sm:flex'>
+        <div className='py-3 sm:flex text-gray-500'>
           <div className='flex flex-col'>
             <div>{date}</div>
             <div>{event.location}</div>
