3ebd604fd36682daca6e0b63d8fde9291052e5f1
Eduard Schwarzkopf <eschwarzkopf@evoila.de>
2023-04-06 16:08:56 +0200

    make use of Box

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -8,7 +8,7 @@ export default function EventCard({ event }: { event: EventType }) {
   const date = new Date(event.date).toLocaleDateString('de-DE');
 
   return (
-    <Box>
+    <Box className=''>
       <div className='mt-3 sm:mt-0'>
         <div className=' text-2xl '>{event.name}</div>
         <div className='mt-2'>
@@ -18,11 +18,12 @@ export default function EventCard({ event }: { event: EventType }) {
           <Link href={`/events/${event.id}`}>Mehr erfahren</Link>
         </div>
         <Button className='w-full'>Registrieren</Button>
-      </div>
-      <div className='py-3 sm:flex text-gray-500'>
-        <div className='flex flex-col'>
-          <div>{date}</div>
-          <div>{event.location}</div>
+
+        <div className='py-3 sm:flex text-gray-500'>
+          <div className='flex flex-col'>
+            <div>{date}</div>
+            <div>{event.location}</div>
+          </div>
         </div>
       </div>
     </Box>
