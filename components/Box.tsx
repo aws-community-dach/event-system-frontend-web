a950338d0fb626f1932169d4bd2aafd38a66099f
Eduard Schwarzkopf <eschwarzkopf@evoila.de>
2023-04-06 16:08:43 +0200

    added styling

--- components/Box.tsx
+++ components/Box.tsx
@@ -3,11 +3,11 @@ export default function Box({
   className,
 }: {
   children: React.ReactNode;
-  className: string;
+  className?: string;
 }) {
   return (
-    <div className={`px-4 bg-white w-full rounded-lg ${className}`}>
-      <div className='pb-4 pt-4 sm:p-6 sm:pb-4'>
+    <div className={`bg-white w-full rounded-lg ${className}`}>
+      <div className='pb-4 sm:p-4 sm:pb-4'>
         <div className='sm:flex sm:items-start'>{children}</div>
       </div>
     </div>
