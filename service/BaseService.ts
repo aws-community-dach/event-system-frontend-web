13ba728ae679658c77da85bc49078cde663cc8bd
Eduard Schwarzkopf <eschwarzkopf@evoila.de>
2023-04-06 16:09:11 +0200

    update for Id's

--- service/BaseService.ts
+++ service/BaseService.ts
@@ -1,25 +1,29 @@
 import request from './request';
 
+type idType = Number | string;
+
 export const BaseService = (endpoint: string) => {
+  const objectURL = (id: idType) => `${endpoint}/${id}`;
+
   return {
     getAll: (parameter: object = {}) => {
       return request.get(endpoint, parameter);
     },
 
-    get: (parameter: object = {}) => {
-      return request.get(endpoint, parameter);
+    get: (id: idType, parameter: object = {}) => {
+      return request.get(objectURL(id), parameter);
     },
 
-    add: (data: object) => {
-      return request.post(endpoint, data);
+    add: (id: idType, data: object) => {
+      return request.post(objectURL(id), data);
     },
 
-    update: (data: object) => {
-      return request.update(endpoint, data);
+    update: (id: idType, data: object) => {
+      return request.update(objectURL(id), data);
     },
 
-    delete: (data: object) => {
-      return request.delete(endpoint, data);
+    delete: (id: idType, data: object) => {
+      return request.delete(objectURL(id), data);
     },
   };
 };
