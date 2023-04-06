286115a8e234c3b87335f656eefd3c91ad653d16
Eduard Schwarzkopf <eschwarzkopf@evoila.de>
2023-04-06 13:54:07 +0200

    Use Card component

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -2,32 +2,29 @@
 import { EventType } from '@/types/EventType';
 import Link from 'next/link';
 import Button from './Button';
+import Card from './Card';
 
 export default function EventCard({ event }: { event: EventType }) {
   const date = new Date(event.date).toLocaleDateString('de-DE');
 
   return (
-    <div className='px-4 bg-white min-h-80 w-full rounded-lg'>
-      <div className='pb-4 pt-5 sm:p-6 sm:pb-4'>
-        <div className='sm:flex sm:items-start'>
-          <div className='mt-3 sm:mt-0'>
-            <div className=' text-2xl '>{event.name}</div>
-            <div className='mt-2'>
-              <p className='text-sm text-gray-500'>{event.summary}</p>
-            </div>
-            <div className='my-6'>
-              <Link href={`/events/${event.id}`}>Mehr erfahren</Link>
-            </div>
-            <Button className='w-full'>Registrieren</Button>
-          </div>
+    <Card>
+      <div className='mt-3 sm:mt-0'>
+        <div className=' text-2xl '>{event.name}</div>
+        <div className='mt-2'>
+          <p className='text-sm text-gray-500'>{event.summary}</p>
         </div>
-        <div className='py-3 sm:flex text-gray-500'>
-          <div className='flex flex-col'>
-            <div>{date}</div>
-            <div>{event.location}</div>
-          </div>
+        <div className='my-6'>
+          <Link href={`/events/${event.id}`}>Mehr erfahren</Link>
+        </div>
+        <Button className='w-full'>Registrieren</Button>
+      </div>
+      <div className='py-3 sm:flex text-gray-500'>
+        <div className='flex flex-col'>
+          <div>{date}</div>
+          <div>{event.location}</div>
         </div>
       </div>
-    </div>
+    </Card>
   );
 }
--- components/EventsForm.tsx
+++ components/EventsForm.tsx
@@ -2,6 +2,7 @@
 
 import { ParticipantService } from '@/service/events/ParticipantService';
 import { ParticipantFormType } from '@/types/ParticipantType';
+import Card from './Card';
 
 export default function EventForm({ eventId }: { eventId: string }) {
   const handleSubmit = (event: React.FormEvent<ParticipantFormType>) => {
@@ -19,17 +20,61 @@ export default function EventForm({ eventId }: { eventId: string }) {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor='name'>Name:</label>
-      <input type='text' id='name' required />
+    <Card>
+      <form className='w-full' onSubmit={handleSubmit}>
+        <div className='grid grid-cols-1 gap-x-6 gap-y-6'>
+          <div>
+            <div className='mt-2'>
+              <input
+                type='text'
+                name='name'
+                id='name'
+                placeholder='Name'
+                autoComplete='given-name'
+                className='pl-2 py-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6'
+              />
+            </div>
+          </div>
 
-      <label htmlFor='displayName'>Display Name:</label>
-      <input type='text' id='displayName' required />
+          <div>
+            <div className='mt-2'>
+              <input
+                type='text'
+                name='displayName'
+                id='displayName'
+                placeholder='Anzeigename'
+                className='pl-2 py-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6'
+              />
+            </div>
+          </div>
 
-      <label htmlFor='email'>Email:</label>
-      <input type='email' id='email' required />
+          <div>
+            <div className='mt-2'>
+              <input
+                id='email'
+                name='email'
+                type='email'
+                placeholder='E-Mail'
+                autoComplete='email'
+                className='pl-2 py-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6'
+              />
+            </div>
+          </div>
+        </div>
 
-      <button type='submit'>Submit</button>
-    </form>
+        <p className='mt-4 text-sm leading-6 text-gray-600'>
+          Es gelten die Datenschutzbestimmungen
+        </p>
+
+        <div className='mt-6 flex items-center justify-end gap-x-6'>
+          <button
+            type='submit'
+            className='rounded-md bg-primary-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600'
+          >
+            Registrieren
+          </button>
+        </div>
+      </form>
+    </Card>
   );
 }
