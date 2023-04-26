b2bfed365bc1dc2d942a32d93260246aa2745dce
Eduard Schwarzkopf <eschwarzkopf@evoila.de>
2023-04-26 16:10:00 +0200

    add location filter

--- app/events/EventsPage.tsx
+++ app/events/EventsPage.tsx
@@ -15,17 +15,47 @@ function fuzzyMatch(searchText: string, textToSearch: string): boolean {
 
 export default function EventsPage({ eventList }: { eventList: EventType[] }) {
   const [search, setSearch] = useState('');
+  const [selectedLocations, setSelectedLocations] = useState<string[]>([]);
+
+  const locations = Array.from(
+    new Set(eventList.map((event) => event.location))
+  );
 
   const filteredEvents = eventList.filter((event) => {
-    const combinedText = event.name;
-    return fuzzyMatch(search, combinedText);
+    const nameMatch = fuzzyMatch(search, event.name);
+    const locationMatch =
+      selectedLocations.length === 0 ||
+      selectedLocations.includes(event.location);
+    return nameMatch && locationMatch;
   });
+
+  const handleLocationCheckboxChange = (location: string, checked: boolean) => {
+    if (checked) {
+      setSelectedLocations([...selectedLocations, location]);
+    } else {
+      setSelectedLocations(selectedLocations.filter((l) => l !== location));
+    }
+  };
 
   return (
     <>
       <div className='w-full flex bg-white'>
         <div className='w-1/4 p-4 border-2 border-red-600'>
-          <h2 className='font-semibold'>Sidebar</h2>
+          <h2 className='font-semibold mb-4'>Standorte</h2>
+          {locations.map((location) => (
+            <div key={location} className='mb-2'>
+              <input
+                type='checkbox'
+                id={location}
+                onChange={(e) =>
+                  handleLocationCheckboxChange(location, e.target.checked)
+                }
+              />
+              <label htmlFor={location} className='ml-2'>
+                {location}
+              </label>
+            </div>
+          ))}
         </div>
         <div className='w-3/4 flex flex-col border-2 border-blue-500'>
           <div className='p-4 border-2 border-green-500'>
@@ -34,7 +64,7 @@ export default function EventsPage({ eventList }: { eventList: EventType[] }) {
               value={search}
               onChange={(e) => setSearch(e.target.value)}
               className='rounded border border-gray-300 py-2 px-3 w-full'
-              placeholder='Search...'
+              placeholder='Suche...'
             />
           </div>
 
