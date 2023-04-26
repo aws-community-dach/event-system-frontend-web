a970af9968b068f3805bafdda79212f10fad2b30
Eduard Schwarzkopf <eschwarzkopf@evoila.de>
2023-04-26 18:46:14 +0200

    fixed item.current

--- app/Nav.tsx
+++ app/Nav.tsx
@@ -135,22 +135,25 @@ export default function Nav({ session }: { session: Session | null }) {
 
           <Disclosure.Panel className='md:hidden'>
             <div className='space-y-1 px-2 pb-3 pt-2 sm:px-3'>
-              {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as='a'
-                  href={item.href}
-                  className={classNames(
-                    item.current
-                      ? 'bg-gray-900 text-white'
-                      : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                    'block rounded-md px-3 py-2 text-base font-medium'
-                  )}
-                  aria-current={item.current ? 'page' : undefined}
-                >
-                  {item.name}
-                </Disclosure.Button>
-              ))}
+              {navigation.map((item) => {
+                const isActive = isCurrentHref(item.href);
+                return (
+                  <Disclosure.Button
+                    key={item.name}
+                    as='a'
+                    href={item.href}
+                    className={classNames(
+                      isActive
+                        ? 'bg-gray-900 text-white'
+                        : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                      'block rounded-md px-3 py-2 text-base font-medium'
+                    )}
+                    aria-current={isActive ? 'page' : undefined}
+                  >
+                    {item.name}
+                  </Disclosure.Button>
+                );
+              })}
             </div>
           </Disclosure.Panel>
         </>
