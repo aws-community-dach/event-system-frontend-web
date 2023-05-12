4904e362b4d721459a91a924105777f0fd838642
Eduard Schwarzkopf <eschwarzkopf@evoila.de>
2023-05-12 09:45:15 +0200

    changing env variable

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,7 +3,7 @@ import CognitoProvider from 'next-auth/providers/cognito';
 
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
-  secret: process.env.SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     CognitoProvider({
       clientId: process.env.COGNITO_CLIENT_ID,
