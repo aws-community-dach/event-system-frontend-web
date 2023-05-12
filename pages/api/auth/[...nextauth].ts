d699745847adea68976dc066a1f90ee89d1fa08a
Eduard Schwarzkopf <eschwarzkopf@evoila.de>
2023-05-12 11:32:34 +0200

    make use of default NEXTAUTH_SECRET

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,8 +2,6 @@ import NextAuth, { NextAuthOptions } from 'next-auth';
 import CognitoProvider from 'next-auth/providers/cognito';
 
 export const authOptions: NextAuthOptions = {
-  // Configure one or more authentication providers
-  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     CognitoProvider({
       clientId: process.env.COGNITO_CLIENT_ID,
