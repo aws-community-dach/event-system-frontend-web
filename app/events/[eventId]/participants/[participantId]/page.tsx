f74c32dab666f4f436718071fb437ff68a17e310
Eduard Schwarzkopf <eschwarzkopf@evoila.de>
2023-06-26 14:45:19 +0200

    fields added

--- app/events/[eventId]/participants/[participantId]/page.tsx
+++ app/events/[eventId]/participants/[participantId]/page.tsx
@@ -6,15 +6,30 @@ import FormInput from '@/components/FormInput';
 import { ParticipantService } from '@/service/events/ParticipantService';
 import { ParticipantType } from '@/types/ParticipantType';
 import FormSuccessFeedback from '@/components/Feedback';
+import FormCheckbox from '@/components/FormCheckbox';
 
-function Page({
+export default function Page({
   params,
 }: {
   params: { eventId: string; participantId: string };
 }) {
   const router = useRouter();
   const [participantData, setParticipantData] =
-    useState<ParticipantType | null>(null);
+    useState<ParticipantType | null>({
+      id: 0,
+      name: '',
+      displayName: '',
+      email: '',
+      customData: {
+        tShirtSize: 'm',
+        eventParticipation: false,
+        foodPreference: 'Meat',
+        jobDescription: '',
+        companyName: '',
+        awsExperience: '< 1 year',
+      },
+    });
+
   const [showFeedback, setShowFeedback] = useState(false);
 
   useEffect(() => {
@@ -22,7 +37,17 @@ function Page({
       const response = await ParticipantService(params.eventId).get(
         params.participantId,
       );
-      setParticipantData(response.data);
+      setParticipantData({
+        ...response.data,
+        customData: response.data.customData ?? {
+          tShirtSize: 'm',
+          eventParticipation: false,
+          foodPreference: 'Meat',
+          jobDescription: '',
+          companyName: '',
+          awsExperience: '< 1 year',
+        },
+      });
     };
 
     fetchParticipantData();
@@ -59,7 +84,6 @@ function Page({
         </FormSuccessFeedback>
       ) : (
         <>
-          <h2>Edit your registration</h2>
           <form className='w-full' onSubmit={handleUpdate}>
             <FormInput
               type='text'
@@ -83,6 +107,124 @@ function Page({
               placeholder='Your name'
               required
             />
+
+            <FormInput
+              as='select'
+              value={participantData.customData.tShirtSize}
+              onChange={(e) =>
+                setParticipantData({
+                  ...participantData,
+                  customData: {
+                    ...participantData.customData,
+                    tShirtSize: e.target.value as
+                      | 'm'
+                      | 'xs'
+                      | 's'
+                      | 'l'
+                      | 'xl'
+                      | 'xxl'
+                      | 'xxxl',
+                  },
+                })
+              }
+              required
+            >
+              <option value='xs'>XS</option>
+              <option value='s'>S</option>
+              <option value='m'>M</option>
+              <option value='l'>L</option>
+              <option value='xl'>XL</option>
+              <option value='xxl'>XXL</option>
+              <option value='xxxl'>XXXL</option>
+            </FormInput>
+
+            <FormCheckbox
+              label='Participation at the evening event'
+              id='eventParticipation'
+              checked={participantData.customData.eventParticipation}
+              onChange={(isChecked) =>
+                setParticipantData({
+                  ...participantData,
+                  customData: {
+                    ...participantData.customData,
+                    eventParticipation: isChecked,
+                  },
+                })
+              }
+            />
+            <FormInput
+              as='select'
+              value={participantData.customData.foodPreference}
+              onChange={(e) =>
+                setParticipantData({
+                  ...participantData,
+                  customData: {
+                    ...participantData.customData,
+                    foodPreference: e.target.value as
+                      | 'Vegan'
+                      | 'Vegetarian'
+                      | 'Meat',
+                  },
+                })
+              }
+              required
+            >
+              <option value='Vegetarian'>Vegetarian</option>
+              <option value='Vegan'>Vegan</option>
+              <option value='Meat'>Meat</option>
+            </FormInput>
+
+            <FormInput
+              type='text'
+              value={participantData.customData.jobDescription}
+              onChange={(e) =>
+                setParticipantData({
+                  ...participantData,
+                  customData: {
+                    ...participantData.customData,
+                    jobDescription: e.target.value,
+                  },
+                })
+              }
+              placeholder='Your job description'
+            />
+            <FormInput
+              type='text'
+              value={participantData.customData.companyName}
+              onChange={(e) =>
+                setParticipantData({
+                  ...participantData,
+                  customData: {
+                    ...participantData.customData,
+                    companyName: e.target.value,
+                  },
+                })
+              }
+              placeholder='Your company name'
+            />
+            <FormInput
+              as='select'
+              value={participantData.customData.awsExperience}
+              onChange={(e) =>
+                setParticipantData({
+                  ...participantData,
+                  customData: {
+                    ...participantData.customData,
+                    awsExperience: e.target.value as
+                      | '< 1 year'
+                      | '1-3 years'
+                      | '3-5 years'
+                      | '5+ years',
+                  },
+                })
+              }
+            >
+              <option value='< 1 year'>{'< 1 year'}</option>
+              <option value='1-3 years'>1-3 years</option>
+              <option value='3-5 years'>3-5 years</option>
+              <option value='+5 years'>5+ years</option>
+            </FormInput>
+
             <div className='flex gap-2'>
               <Button type='submit'>Update Registration</Button>
 
@@ -96,5 +238,3 @@ function Page({
     </>
   );
 }
-
-export default Page;
