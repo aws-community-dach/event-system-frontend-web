26f9ad8eb5e182944b271bd8fbbee41c00ff40fb
Eduard Schwarzkopf <eschwarzkopf@evoila.de>
2023-06-26 14:37:34 +0200

    select added

--- components/FormInput.tsx
+++ components/FormInput.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 
 // prettier-ignore
-type FormInputProps = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> & { label?: string };
+type InputProps = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>;
+// prettier-ignore
+type SelectProps = React.DetailedHTMLProps<React.SelectHTMLAttributes<HTMLSelectElement>, HTMLSelectElement>;
+
+type FormInputProps = InputProps &
+  SelectProps & { as?: 'input' | 'select'; label?: string };
 
 export default function FormInput({
+  as = 'input',
   label,
   id,
   name,
-  type,
   placeholder,
   required,
+  children,
   ...props
 }: FormInputProps) {
   return (
@@ -20,15 +26,26 @@ export default function FormInput({
         </label>
       )}
       <div className='mt-2'>
-        <input
-          id={id}
-          name={name}
-          type={type}
-          placeholder={placeholder}
-          required={required}
-          className='pl-2 py-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-accent-600 sm:text-sm sm:leading-6'
-          {...props}
-        />
+        {as === 'input' ? (
+          <input
+            id={id}
+            name={name}
+            placeholder={placeholder}
+            required={required}
+            className='pl-2 py-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-accent-600 sm:text-sm sm:leading-6'
+            {...props}
+          />
+        ) : (
+          <select
+            id={id}
+            name={name}
+            required={required}
+            className='pl-2 py-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-accent-600 sm:text-sm sm:leading-6'
+            {...props}
+          >
+            {children}
+          </select>
+        )}
       </div>
     </div>
   );
