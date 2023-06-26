16d025ca4416ecb983d83bdd0c97d0aaf242f9de
Eduard Schwarzkopf <eschwarzkopf@evoila.de>
2023-06-26 18:34:33 +0200

    refactored for new component

--- app/events/[eventId]/participants/[participantId]/page.tsx
+++ app/events/[eventId]/participants/[participantId]/page.tsx
@@ -1,61 +1,29 @@
-'use client';
-import { useState, useEffect } from 'react';
+import ParticipantFormUpdate from '@/components/ParticipantFormUpdate';
 import { ParticipantService } from '@/service/events/ParticipantService';
 import { ParticipantType } from '@/types/ParticipantType';
-import ParticipantForm from '@/components/ParticpantForm';
 
-export default function Page({
+async function getData(eventId: string, participantId: string) {
+  const res = await ParticipantService(eventId).get(participantId);
+
+  return res.data;
+}
+
+export default async function Page({
   params,
 }: {
   params: { eventId: string; participantId: string };
 }) {
-  const [participantData, setParticipantData] =
-    useState<ParticipantType | null>({
-      id: 0,
-      name: '',
-      displayName: '',
-      email: '',
-      customData: {
-        tShirtSize: 'm',
-        eventParticipation: false,
-        foodPreference: 'Meat',
-        jobDescription: '',
-        companyName: '',
-        awsExperience: '< 1 year',
-      },
-    });
-
-  useEffect(() => {
-    const fetchParticipantData = async () => {
-      const response = await ParticipantService(params.eventId).get(
-        params.participantId,
-      );
-      setParticipantData({
-        ...response.data,
-        customData: response.data.customData ?? {
-          tShirtSize: 'm',
-          eventParticipation: false,
-          foodPreference: 'Meat',
-          jobDescription: '',
-          companyName: '',
-          awsExperience: '< 1 year',
-        },
-      });
-    };
-
-    fetchParticipantData();
-  }, [params.eventId, params.participantId]);
-
-  if (!participantData) return <div>Loading...</div>; // Add loading state
-
+  const participant: ParticipantType = await getData(
+    params.eventId,
+    params.participantId,
+  );
   return (
     <>
       <h1>Update your Registration</h1>
       <>
-        <ParticipantForm
-          participantData={participantData}
+        <ParticipantFormUpdate
           eventId={params.eventId}
-          isEditMode={true}
+          participantData={participant}
         />
       </>
     </>
