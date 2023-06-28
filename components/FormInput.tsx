28ffb511e915b72ad50fdae20befc1cfbfb9c723
Eduard Schwarzkopf <eschwarzkopf@evoila.de>
2023-06-28 14:53:46 +0200

    some style

--- components/FormInput.tsx
+++ components/FormInput.tsx
@@ -36,7 +36,7 @@ export default function FormInput({
             name={name}
             placeholder={placeholder}
             required={required}
-            className='pl-2 py-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-accent-600 sm:text-sm sm:leading-6'
+            className='pl-2 py-3 block w-full bg-gray-50 rounded-md py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6'
             {...props}
           />
         ) : (
@@ -44,7 +44,7 @@ export default function FormInput({
             id={id}
             name={name}
             required={required}
-            className='pl-2 py-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-accent-600 sm:text-sm sm:leading-6'
+            className='pl-2 py-3 block w-full rounded-md py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6'
             {...props}
           >
             {children}
