0871f6d95f8f8b3f8914369e6f8057912e489ae0
Eduard Schwarzkopf <eschwarzkopf@evoila.de>
2023-06-28 15:35:09 +0200

    work with tokens

--- app/events/[eventId]/participants/[participantId]/page.tsx
+++ app/events/[eventId]/participants/[participantId]/page.tsx
@@ -1,22 +1,55 @@
 import ParticipantFormUpdate from '@/components/ParticipantFormUpdate';
+import { ErrorFeedback } from '@/components/Feedback';
 import { ParticipantService } from '@/service/events/ParticipantService';
 import { ParticipantType } from '@/types/ParticipantType';
 
-async function getData(eventId: string, participantId: string) {
-  const res = await ParticipantService(eventId).get(participantId);
-
-  return res.data;
+async function getData(eventId: string, participantId: string, token: string) {
+  try {
+    const res = await ParticipantService(eventId).get(
+      `${participantId}?token=${token}`,
+    );
+    return res.data;
+  } catch (error) {
+    return null;
+  }
 }
 
 export default async function Page({
   params,
+  searchParams,
 }: {
   params: { eventId: string; participantId: string };
+  searchParams: { token: string };
 }) {
+  const token = searchParams.token;
+
+  if (!token) {
+    return (
+      <ErrorFeedback title='Kein Token'>
+        Es wurde leider kein Token angegeben
+      </ErrorFeedback>
+    );
+  }
+
   const participant: ParticipantType = await getData(
     params.eventId,
     params.participantId,
+    token,
   );
+
+  if (participant === null) {
+    return (
+      <>
+        <ErrorFeedback title='Teilnehmer nicht gefunden'>
+          <p>Bitte prüfe deine Eingaben und versuche es erneut.</p>
+          <div className='mt-6'>
+            <a href={`/events/${params.eventId}/participants/edit`}>Zurück</a>
+          </div>
+        </ErrorFeedback>
+      </>
+    );
+  }
+
   return (
     <>
       <h1>Update your Registration</h1>
