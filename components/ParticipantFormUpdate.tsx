cd42da3a85590b2d9277795fa26960dde1288f87
Eduard Schwarzkopf <eschwarzkopf@evoila.de>
2024-05-10 15:20:48 +0200

    no need to set delete state back to false

--- components/ParticipantFormUpdate.tsx
+++ components/ParticipantFormUpdate.tsx
@@ -57,7 +57,6 @@ export default function ParticipantFormUpdate({
 
     setIsDeleted(true);
     setTimeout(() => {
-      setIsDeleted(false);
       router.push(`/events/${eventId}`);
     }, 3000);
   };
